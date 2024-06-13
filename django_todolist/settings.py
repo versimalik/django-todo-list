d31b2cc11afaaedeb24e73977d89527bc5589065
Reza Malik <rezamalikdrive@gmail.com>
2024-06-13 21:37:10 +0700

    email feature added

--- django_todolist/settings.py
+++ django_todolist/settings.py
@@ -123,5 +123,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your@email.com'
+EMAIL_HOST_PASSWORD = 'your-app-password'
--- main/views.py
+++ main/views.py
@@ -3,6 +3,10 @@ from django.urls import reverse
 from .models import Todo
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
 # Create your views here.
 @login_required
@@ -21,6 +25,18 @@ def create(request):
     create_todo = Todo(text = todo_text, due_date = todo_due_date, user = user)
     create_todo.save()
 
+    context = {'todo': create_todo}
+    html_message = render_to_string('main/email/todo_added.html', context)
+
+    send_mail(
+        subject="[no-reply]New Todo Added",
+        message=strip_tags(html_message),
+        from_email = settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email],
+        html_message=html_message,
+        fail_silently=False
+    )
+
     return HttpResponseRedirect(reverse('todo-index'))
 
 def update(request, todo_id):
@@ -28,11 +44,41 @@ def update(request, todo_id):
     todo.status = not todo.status
     todo.save()
 
+    todo_status_message = None
+    if todo.status:
+        todo_status_message = "Done"
+    else:
+        todo_status_message = "Not Done"
+
+    message = "New Todo is updated!\n"  
+    message += f"{todo.text} is set to {todo_status_message}\n"
+    message += f"Due date: {todo.due_date}"
+
+    send_mail(
+        subject="[no-reply]New Todo Updated!",
+        message=message,
+        from_email = settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email],
+        fail_silently=False
+    )
+
     return HttpResponseRedirect(reverse('todo-index'))
 
 def delete(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     todo.delete()
+
+    message = "Todo is deleted!\n"  
+    message += f"{todo.text} is deleted\n"
+    message += f"Due date: {todo.due_date}"
+
+    send_mail(
+        subject="[no-reply]New Todo Deleted!",
+        message=message,
+        from_email = settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email],
+        fail_silently=False
+    )
 
     return HttpResponseRedirect(reverse('todo-index'))
     
