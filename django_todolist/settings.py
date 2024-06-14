38e074a66070b9f7df8551222143e8ae3deac05a
Reza Malik <rezamalikdrive@gmail.com>
2024-06-14 22:12:55 +0700

    messages and admin site added

--- django_todolist/settings.py
+++ django_todolist/settings.py
@@ -132,8 +132,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rezamalikdrive@gmail.com'
-EMAIL_HOST_PASSWORD = 'skydxxpymsdqhiiz'
+EMAIL_HOST_USER = 'email@email.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxx'
 
 MESSAGE_TAGS = {
     messages.DEBUG : 'alert alert-secondary',
